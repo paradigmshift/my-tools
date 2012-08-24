a5aee2a034f74cdbc0da630641657cf462c71576
Mozart Reina <mo.opensource@gmail.com>
2012-08-24 11:20:25 +0900

    -*- mode: org -*- #+STARTUP: showeverything * CHANGES ** list-factor test fixed
    * TODO
    - [ ] rewrite permute function to accept arrays
    - [X] fix 'list-factors test

--- my-tools-tests.lisp
+++ my-tools-tests.lisp
@@ -1,16 +1,15 @@
 (in-package #:my-tools-tests)
 
 (define-test list-switch
-    (assert-equal '(1 0 2 3) (list-switch 0 1 '(0 1 2 3))))
+  (assert-equal '(1 0 2 3) (list-switch 0 1 '(0 1 2 3))))
 
 (define-test permute
-    (assert-equal 120 (length (permute '(0 1 2 3 4)))))
+  (assert-equal 120 (length (permute '(0 1 2 3 4)))))
 
-;;; needs work
-;;(define-test array-switch
-;;    (assert-equal '#(1 0 2 3) (array-switch 0 1 '#(0 1 2 3))))
+(define-test array-switch
+  (assert-equalp #(1 0 2 3) (array-switch 0 1 #(0 1 2 3))))
 
 (define-test list-factors
-    (assert-equal 13195 (reduce #'* (list-factors 13195)))
-    (assert-equal '(5 2 2) (list-factors 20)))
+  (assert-equal 13195 (reduce #'* (list-factors 13195)))
+  (assert-equal '(5 2 2) (list-factors 20)))
   
